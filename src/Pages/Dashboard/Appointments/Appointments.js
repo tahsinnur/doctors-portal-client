1d174a6f353ece837d3ac8cb1fd00b07eabe1d19
tahsinnur <tahsinnurtajin@gmail.com>
2021-11-10 00:18:06 +0600

    date fixing

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({date}) => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const url = `https://gentle-savannah-03074.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url = `https://gentle-savannah-03074.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers:{
                 'authorization': `Bearer ${token}`
